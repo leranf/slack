2559dfd33275f8ba1977654797f653bbaf7c5789
Leran Firer <leranf@gmail.com>
2016-08-07 16:47:40 -0700

    disables previous and next buttons under certain conditions

--- scripts/eventListeners.js
+++ scripts/eventListeners.js
@@ -1,7 +1,7 @@
 document.querySelector('#search').addEventListener('click', function() {
   httpRequest = new XMLHttpRequest();
   httpRequest.onreadystatechange = renderImages;
-  httpRequest.open('GET', `https://api.flickr.com/services/rest?api_key=${API_KEY}&method=flickr.photosets.getPhotos&user_id=${USER_ID}&photoset_id=72157631585502446&format=json&per_page=18`);
+  httpRequest.open('GET', `https://api.flickr.com/services/rest?api_key=${API_KEY}&method=flickr.photosets.getPhotos&user_id=${USER_ID}&photoset_id=72157671032461750&format=json&per_page=18`);
   httpRequest.send(null);
 });
 
@@ -10,9 +10,15 @@ document.querySelector('.selected_image').addEventListener('click', function() {
 });
 
 document.querySelector('#previous').addEventListener('click', function() {
+  if (gridPosition[0] === '2' && gridPosition[1] === '5') {
+    document.querySelector('#next').style.display = 'inline';
+  }
   if (Number(gridPosition[1]) > 0) {
     gridPosition[1] = String(Number(gridPosition[1] - 1));
     updateSelectedImage(document.getElementById(gridPosition.join('_')));
+    if (gridPosition[0] === '0' && gridPosition[1] === '0') {
+      document.querySelector('#previous').style.display = 'none';
+    }
   } else {
     if (Number(gridPosition[0]) > 0) {
       gridPosition[0] = `${Number(gridPosition[0] - 1)}`;
@@ -23,9 +29,15 @@ document.querySelector('#previous').addEventListener('click', function() {
 });
 
 document.querySelector('#next').addEventListener('click', function() {
+  if (gridPosition[0] === '0' && gridPosition[1] === '0') {
+    document.querySelector('#previous').style.display = 'inline';
+  }
   if (Number(gridPosition[1]) < 5) {
     gridPosition[1] = String(Number(gridPosition[1]) + 1);
     updateSelectedImage(document.getElementById(gridPosition.join('_')));
+    if (gridPosition[0] === '2' && gridPosition[1] === '5') {
+      document.querySelector('#next').style.display = 'none';
+    }
   } else {
     if (Number(gridPosition[0]) < 2) {
       gridPosition[0] = `${Number(gridPosition[0]) + 1}`;
@@ -40,5 +52,10 @@ function addThumbnailImageEventListener(image) {
     gridPosition = image.id.split('_');
     updateSelectedImage(image);
     document.querySelector('#lightbox').style.display = 'inline';
+    if (gridPosition[0] === '0' && gridPosition[1] === '0') {
+      document.querySelector('#previous').style.display = 'none';
+    } else if (gridPosition[0] === '2' && gridPosition[1] === '5') {
+      document.querySelector('#next').style.display = 'none';
+    }
   });
 }
