bda7930fbf3178ff6982fdb7409d82d4aa089e4b
Leran Firer <leranf@gmail.com>
2016-08-10 22:20:56 -0700

    simplifies logic to store data from API call in arrray of objects and create a new li from each item

--- scripts/main.js
+++ scripts/main.js
@@ -1,42 +1,24 @@
 var httpRequest;
-var photoInfo = [ [], [], [] ];
-var gridPosition;
+var photoInfo = [];
 
 function renderImages() {
   if (httpRequest.readyState === 4) {
     if (httpRequest.status === 200) {
       var results = JSON.parse(httpRequest.responseText.slice(14).slice(0,-1)).photoset.photo;
-      var count = 0;
-      var currentRow = 0;
       
       results.forEach(function(result) {
-        if (count < 6) {
-          photoInfo[currentRow].push({
-            url: `https://farm${result.farm}.staticflickr.com/${result.server}/${result.id}_${result.secret}.jpg`,
-            title: result.title
-          });
-          count++;
-        } else {
-          photoInfo[++currentRow].push({
-            url: `https://farm${result.farm}.staticflickr.com/${result.server}/${result.id}_${result.secret}.jpg`,
-            title: result.title
-          });
-          count = 1;
-        }
+        photoInfo.push({
+          url: `https://farm${result.farm}.staticflickr.com/${result.server}/${result.id}_${result.secret}.jpg`,
+          title: result.title
+        });
       });
       
-      if ('content' in document.createElement('template')) {
-        var t = document.querySelector('#image_row');
-        var td = t.content.querySelectorAll('td');
-        photoInfo.forEach(function(row, rowIndex) {
-          row.forEach(function(item, columnIndex) {
-            td[columnIndex].innerHTML = `<img id='${rowIndex}_${columnIndex}' class='flickr_image' src='${item.url}' alt='${item.title}'>`;
-          });
-          var table = document.querySelector('#images');
-          var clone = document.importNode(t.content, true);
-          table.appendChild(clone);
-        });
-      }
+      var list = document.querySelector('#images');
+      photoInfo.forEach(function(photo, i) {
+        var li = document.createElement('li');
+        li.innerHTML = `<img id='${i}' class='flickr_image' src='${photo.url}' alt='${photo.title}'>`;
+        list.appendChild(li);
+      }); 
       
       document.querySelectorAll('.flickr_image').forEach(function(image) {
         addThumbnailImageEventListener(image);
